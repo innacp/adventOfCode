580f1763fbb7cfd403e8f039e1f93b1bd5c16a1e
innacp <inna.chapkevych@gmail.com>
2025-01-05 17:28:07 +0200

    nailed third star

--- 2024/day1/index.js
+++ 2024/day1/index.js
@@ -5,8 +5,6 @@ async function main() {
   const columns = [[], []]; // 1st element of splittedNumbers | 2nd
 
   for (let line of lines) {
-    //80421   40193
-
     let splittedNumbers = line.split(/\s+/);
 
     columns[0].push(parseInt(splittedNumbers[0]));
@@ -22,68 +20,29 @@ async function main() {
     totalDistance += Math.abs(columns[0][index] - columns[1][index]);
   }
 
-  // for (let index = 0; index < columns[0].length; index++) {
-  //   if (columns[0][index] === columns[1][index]) {
-  //     console.log(columns[1][index]);
-  //   }
-  // }
-
-  // const countOccurrences = (arr1, arr2) => {
-  //   const firstColumn = columns[0];
-  //   const secondColumn = columns[1];
-  //   const counts = arr1.reduce((acc, el) => {
-  //     acc[el] = arr2.filter((item) => item === el).length;
-  //     return acc;
-  //   }, {});
-  //   return counts;
-  // };
-  // console.log(countOccurrences(firstColumn, secondColumn));
-
-  // const firstColumn = columns[0];
-  // const secondColumn = columns[1];
-
-  // const frequencyMap = secondColumn.reduce((acc, num) => {
-  //   const key = Number(num);
-  //   acc[key] = (acc[key] || 0) + 1;
-  //   return acc;
-  // }, {});
-  // console.log("frequency map", frequencyMap);
-
-  // const totalSimilarityScore =
-  //   ((score, num) => {
-  //     const count = frequencyMap[num] || 0;
-  //     return score + num * count;
-  //   },
-  //   0);
-  // console.log(totalSimilarityScore);
-
   const leftList = columns[0];
   const rightList = columns[1];
 
   function calculateSimilarityScore(leftList, rightList) {
-    // Create a map to count occurrences in the right list
     const countMap = new Map();
 
-    // Count occurrences of each number in the right list
     rightList.forEach((num) => {
       countMap.set(num, (countMap.get(num) || 0) + 1);
     });
 
     console.log(countMap);
 
-    // Calculate the total similarity score
     let totalScore = 0;
     leftList.forEach((num) => {
-      const count = countMap.get(num) || 0; // Get the count from the map
-      totalScore += num * count; // Multiply and add to the total score
+      const count = countMap.get(num) || 0;
+      totalScore += num * count;
     });
 
     return totalScore;
   }
 
-  // Example usage
   const similarityScore = calculateSimilarityScore(leftList, rightList);
 
-  console.log(similarityScore); // Output: 14
+  console.log(similarityScore);
 }
 main();
--- 2024/day2/index.js
+++ 2024/day2/index.js
@@ -1,1 +1,48 @@
 import { readFile } from "../../utils/file.js";
+
+async function main() {
+  const lines = await readFile("./input.txt");
+
+  const matrix = lines.map((line) =>
+    line.split(" ").map((string) => parseInt(string))
+  );
+
+  let safeArrays = 0;
+
+  matrix.forEach((array) => {
+    let isSafe = true;
+
+    let ascending = true;
+    let descending = true;
+
+    // let remainder = 0;
+
+    //   for (let i = 1; i < array.length && (ascending || descending); i++) {
+    //     ascending = ascending && array[i] >= array[i - 1];
+    //     descending = descending && array[i] <= array[i - 1];
+    //     orderedArrays;
+    //     remainder = array[i] - array[i - 1];
+    //   }
+
+    //   if ((ascending || descending) && (remainder >= 1 || remainder <= 3)) {
+    //     safeArrays++;
+    //   }
+    // });
+
+    for (let i = 1; i < array.length; i++) {
+      const difference = Math.abs(array[i] - array[i - 1]);
+      if (difference < 1 || difference > 3) {
+        isSafe = false;
+        break;
+      }
+      ascending = ascending && array[i] >= array[i - 1];
+      descending = descending && array[i] <= array[i - 1];
+    }
+
+    if (isSafe && (ascending || descending)) {
+      safeArrays++;
+    }
+  });
+  console.log(safeArrays);
+}
+main();
